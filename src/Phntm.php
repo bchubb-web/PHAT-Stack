b584377075f8ff83d90f2b42dc8746539d8ad428
Billy Chubb <billy@bchubb.co.uk>
2024-03-29 20:37:30 +0000

    added phntm master class to handle core services

---
+++ src/Phntm.php
@@ -0,0 +1,64 @@
+<?php 
+
+namespace bchubbweb\phntm;
+
+use bchubbweb\phntm\Routing\Router;
+use bchubbweb\phntm\Profiling\Profiler;
+
+final class Phntm
+{
+    private static ?Phntm $instance = null;
+    private static ?Router $routerInstance = null;
+    private static ?Profiler $profilerInstance = null;
+    private $config = [];
+
+    private function __construct()
+    {
+        $this->config = Config::get('config');
+    }
+
+    /**
+     * Get the instance of the Phntm class
+     *
+     * @return Phntm
+     */
+    public static function getInstance(): Phntm
+    {
+        if (null === self::$instance) {
+            self::$instance = new Phntm();
+        }
+        return self::$instance;
+    }
+
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+
+    /**
+     * Get the router instance
+     *
+     * @return Router
+     */
+    public static function Router(): Router
+    {
+        if (null === self::$routerInstance) {
+            self::$routerInstance = new Router();
+        }
+        return self::$routerInstance;
+    }
+    /**
+     * Start the profiler, returns false if the profiler is already running
+     *
+     * @return bool
+     */
+    public static function Profile(): bool
+    {
+        if (null === self::$profilerInstance) {
+            self::$profilerInstance = new Profiler();
+            self::$profilerInstance->start();
+            return true;
+        }
+        return false;
+    }
+}
