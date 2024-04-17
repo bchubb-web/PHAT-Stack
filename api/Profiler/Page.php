3b3037357590de14cd8a83770b25dba1b5926670
Billy Chubb <billy@bchubb.co.uk>
2024-04-17 13:02:46 +0100

    using emitter and a few changes to http method calling

--- api/Profiler/Page.php
+++ api/Profiler/Page.php
@@ -4,13 +4,16 @@ namespace Pages\Api\Profiler;
 
 use bchubbweb\phntm\Resources\Page as PageTemplate;
 use bchubbweb\phntm\Phntm;
-use bchubbweb\phntm\Routing\Param;
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+
 
 class Page extends PageTemplate
 {
-    public function get(Param $param): void
+    public function get(Request $request, Response $response, array $params): Response
     {
         $this->setContent(Phntm::Redis()->get('phntm_profiling'));
+
+        return $response;
     }
 }
